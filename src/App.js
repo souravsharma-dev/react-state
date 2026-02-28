f291c4d1781f39cc1dbe6e1c79ad026ba15e3e01
souravsharma-dev <sourav.sourav.sharma07@gmail.com>
2026-02-28 22:45:57 +0530

    change in UI

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
   return (
     <div style={
       {minHeight:"100vh",
-        background: "lightgrey",
+        background: "grey",
         textAlign: "center",
       }
     }>
