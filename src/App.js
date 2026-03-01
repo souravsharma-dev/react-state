653cac7bc07e8bf1b36897b6165f1b85bd7f3818
souravsharma-dev <sourav.sourav.sharma07@gmail.com>
2026-03-01 22:56:00 +0530

    change in UI

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
 
    const [FontSize, setFontSize] = useState('');
 
-  const [colorBox, setColorBox] = useState('orange');
+  const [colorBox, setColorBox] = useState('blue');
  
 
   return (
