be8b307bd6334fb890ab82e5068c9c595d6d3494
souravsharma-dev <sourav.sourav.sharma07@gmail.com>
2026-03-17 22:00:04 +0530

    change in UI

--- src/App.js
+++ src/App.js
@@ -117,7 +117,7 @@ const App = () => {
         <div style={{
         margin : '0 auto',
         width: '50%',
-        background: 'red',
+        background: 'orange',
         padding: '20px',
       }}>
         
